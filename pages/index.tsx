bf3ccd1409eddab57c08ca7cafd58a0d27d16ef6
Gaurish Gangwar <gaurishgangwar@gmail.com>
2023-06-28 16:34:46 +0000

    post card styling done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
 import { Inter } from 'next/font/google';
 import Header from '@/components/header';
 import { GetServerSideProps } from 'next';
-import { sanityClient } from '@/sanity';
+import { sanityClient, urlFor } from '@/sanity';
 import { Post } from '@/typings';
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -32,7 +33,30 @@ export default function Home({posts}: Props) {
     </div>
 
     {/** POSTS */}
+    <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6'>
+      {posts.map(post => (
+        <Link key={post._id} href={`/post/${post.slug.current}`}>
+          <div className='border rounded-lg group cursor-pointer overflow-hidden'>
+            {post.mainImage && 
+              <img
+                className='h-60 w-full object-cover group-hover:scale-105 transition duration-200 ease-in-out'
+                src={urlFor(post.mainImage).url()}
+              />
+            }
+            <div className='flex justify-between p-5 bg-white'>
+              <div>
+                <p className='text-lg font-bold'>{post.title}</p>
+                <p className='text-xs'>{post.description} by {post.author.name}</p>
+              </div>
 
+              <img 
+                className='h-12 w-12 rounded-full'
+                src={urlFor(post.author.image).url()} alt="" />
+            </div>
+          </div>
+        </Link>
+      ))}
+    </div>
   </div>);
 }
 
@@ -50,7 +74,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
   }`;
   
   const posts: Post[] = await sanityClient.fetch(query);
-  console.log(posts);
   return {
     props: {posts}
   };
